3f43f9e458340e7560807d758ef5f404ec4c69ce
kaijunna <shell1016221@gmail.com>
2020-03-07 14:41:58 +0900

    no message

--- config.php
+++ config.php
@@ -4,7 +4,8 @@ define('DSN','mysql:host=localhost;dbname=amshop');
 define('DB_USER','staff');
 define('DB_PASS','pass12');
 define('DB_sock','unix_socket=/tmp/mysql.sock')
-$pdo = new PDO(DSN, DB_sock, DB_USER, DB_PASS);
+define('DB_cha','charset=utf8')
+$pdo = new PDO(DSN, DB_cha,DB_sock, DB_USER, DB_PASS);
 
 
 ?>
