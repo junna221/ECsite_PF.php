f1bc85c08fd8853af3c094cfbeabe3e641f0f2a5
kaijunna <shell1016221@gmail.com>
2020-03-07 12:00:54 +0900

    no message

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 define('DSN','mysql:host=127.0.0.1;dbname=amshop');
-define('DB_USER','sta');
+define('DB_USER','user');
 define('DB_PASS','pass12');
 
 $pdo = new PDO(DSN, DB_USER, DB_PASS);
