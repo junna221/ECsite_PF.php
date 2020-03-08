ed1e67ea8403d0e645f7ff68c0a1640d30d52b65
kaijunna <shell1016221@gmail.com>
2020-03-08 12:25:23 +0900

    no message

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DSN','mysql:host=localhost;dbname=amshop; ;charset=utf8 unix_socket = / Applications / MAMP / tmp / mysql / mysql.sock');
+define('DSN','mysql:host=127.0.0.1; dbname=amshop; ;charset=utf8');
 define('DB_USER','staff');
 define('DB_PASS','pass12');
 
