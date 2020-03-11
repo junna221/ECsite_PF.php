c51a049ef2c011657d51768fd3369bb93c950fe1
kaijunna <shell1016221@gmail.com>
2020-03-11 14:10:10 +0900

    no message

--- config.php
+++ config.php
@@ -7,17 +7,19 @@ define('DB_PASS','pass12');
 $pdo = new PDO(DSN,DB_USER,DB_PASS);
 
 function dbConnect(){
-$db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
-  $db['amshop'] = ltrim($db['http://localhost:8888/phpMyAdmin/db_structure.php?server=1&db=amshop'], '/');
-  $dsn = "mysql:host={$db['localhost']};amshop={$db['amshop']};charset=utf8";
-  $user = $db['staff'];
-  $password = $db['pass12'];
+  $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+  $db['dbname'] = ltrim($db['path'], '/');
+  $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
+  $user = $db['user'];
+  $password = $db['pass'];
   $options = array(
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
   );
-  $pdo = new PDO($dsn,$user,$password,$options);
+  $dbh = new PDO($dsn,$user,$password,$options);
+  return $dbh;
 }
+
 
 ?>
