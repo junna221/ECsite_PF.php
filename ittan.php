e68f556f479fe60fe0d5f42de7657a9440fabf8c
kaijunna <shell1016221@gmail.com>
2020-03-11 14:01:22 +0900

    no message

--- ittan.php
+++ ittan.php
@@ -1,5 +1,4 @@
-function dbConnect(){
-  $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+ $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
   $db['amshop'] = ltrim($db['http://localhost:8888/phpMyAdmin/db_structure.php?server=1&db=amshop'], '/');
   $dsn = "mysql:host={$db['localhost']};amshop={$db['amshop']};charset=utf8";
   $user = $db['staff'];
@@ -9,9 +8,8 @@ function dbConnect(){
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
   );
-  $dbh = new PDO($dsn,$user,$password,$options);
-  return $dbh;
-}
+  $pdo = new PDO($dsn,$user,$password,$options);
+
 
 
 
