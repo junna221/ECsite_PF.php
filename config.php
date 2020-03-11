b8050f43dba573c5135580cbc79d98032547e7c6
kaijunna <shell1016221@gmail.com>
2020-03-11 14:11:06 +0900

    no message

--- config.php
+++ config.php
@@ -17,8 +17,8 @@ function dbConnect(){
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
   );
-  $dbh = new PDO($dsn,$user,$password,$options);
-  return $dbh;
+  $pdo = new PDO($dsn,$user,$password,$options);
+  return $pdo;
 }
 
 
