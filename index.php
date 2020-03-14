e4e081029ea04e3ff7d13b56cf688bc03724a185
kaijunna <shell1016221@gmail.com>
2020-03-14 18:06:00 +0900

    no message

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 //ini_set("display_errors", 'On');
 //error_reporting(E_ALL);
-include('config.php');
+require('config.php');
 //新商品が登録された順に表示するsql
 $sql = "select * from product WHERE id <= 200 ORDER BY id desc";
 $stmt = $pdo->query($sql);
