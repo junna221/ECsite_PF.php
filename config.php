a6727c278d68edc77715ee61cb2ef4dfc6580102
kaijunna <shell1016221@gmail.com>
2020-03-14 12:31:46 +0900

    no message

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-if(!(parse_url($_SERVER['CLEARDB_DATABASE_URL']))){
+if(empty(parse_url($_SERVER['CLEARDB_DATABASE_URL']))){
 $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
   $db['dbname'] = ltrim($db['path'], '/');
   $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
