413a9fda96aae4d9ffd82ba4693abb2ee0519572
kaijunna <shell1016221@gmail.com>
2020-03-16 11:54:37 +0900

    no message

--- account-in.php
+++ account-in.php
@@ -41,16 +41,18 @@ if (!filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL)) {
 }
 
 //DB内でPOSTされたメールアドレスを検索
-
+try {
   $stmt = $pdo->prepare('select * from customer where mail = ?');
   //$stmt->bindValue(1,$mail);
   $stmt->execute([$_POST['mail']]);
   //$row = $stmt->fetch(PDO::FETCH_ASSOC);
-
-
+} catch (\Exception $e) {
+  echo $e->getMessage() . PHP_EOL;
+}
+  
 foreach ($stmt as $row) {
 	$_SESSION['customer']=[
-		'id'=>$row['id'], 'name'=>$row['name'], 'mail'=>$row['mail'],
+		'id'=>$row['id'], 'name'=>$row['name'],
 		'address'=>$row['address']];
 }
 
@@ -63,7 +65,7 @@ if (!isset($row['mail'])) {
 if (password_verify($_POST['password'], $row['password'])) {
   session_regenerate_id(true); //session_idを新しく生成し、置き換える
   $_SESSION['mail'] = $row['mail'];
-  header('Location: http://localhost:8888/ECsite_PF.php');
+  header('Location: /ECsite_PF.php');
   exit();
 } else {
   echo 'メールアドレス又はパスワードが間違っています。';
--- ittan.php
+++ ittan.php
@@ -1,20 +1,2 @@
- $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
-  $db['amshop'] = ltrim($db['http://localhost:8888/phpMyAdmin/db_structure.php?server=1&db=amshop'], '/');
-  $dsn = "mysql:host={$db['localhost']};amshop={$db['amshop']};charset=utf8";
-  $user = $db['staff'];
-  $password = $db['pass12'];
-  $options = array(
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
-  );
-  $pdo = new PDO($dsn,$user,$password,$options);
-
-
-
-
-define('DSN','mysql:host=localhost;dbname=amshop;charset=utf8;');
-define('DB_USER','staff');
-define('DB_PASS','pass12');
-
-$pdo = new PDO(DSN,DB_USER,DB_PASS);
+header('Location: http://localhost:8888/ECsite_PF.php');
+  exit();
