87f4de962aafad7653a55fde509657635c2a7b01
kaijunna <shell1016221@gmail.com>
2020-03-16 12:18:14 +0900

    no message

--- account-in.php
+++ account-in.php
@@ -67,6 +67,8 @@ if (password_verify($_POST['password'], $row['password'])) {
   $_SESSION['mail'] = $row['mail'];
  header('Location: https://shopping-site-php.herokuapp.com/');
   exit();
+  header('Location: http://localhost:8888/ECsite_PF.php');
+  exit();
 } else {
   echo 'メールアドレス又はパスワードが間違っています。';
   return false;
