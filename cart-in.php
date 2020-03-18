4e7590a8ebd54ffc87fdadd2f20d5065e8c2fc5a
kaijunna <shell1016221@gmail.com>
2020-03-18 21:48:14 +0900

    no message

--- cart-in.php
+++ cart-in.php
@@ -19,6 +19,15 @@ $_SESSION['product'][$id]=[
   'price'=>$_POST['price'], 
   'num'=>$count+$_POST['num']
 ];
+//商品数を更新
+$a = ('update cart set  count = :count where customer_id =:customer_id and product_id =:product_id');
+  $a = $pdo->prepare($a);
+  $i = array(':count' =>$_POST['num'],':customer_id'=>$_SESSION['customer']['id'],':product_id'=>$_POST['id']);
+  //$a->bindValue(':count',$count);
+  //$a->bindValue(':customer_id',$customer_id);
+  //$a->bindValue('::product_id',$product_id);
+  $a->execute($i);
+  
 $sql=$pdo->prepare('insert into cart values(?,?,?)');
   $sql->execute([$_SESSION['customer']['id'],$_POST['id'],$_POST['num']]);
 echo '<p>カートに商品を追加しました。</p>';
