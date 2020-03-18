ae57111d96af35a426b36dc1806a0498184b572c
kaijunna <shell1016221@gmail.com>
2020-03-18 09:59:41 +0900

    no message

--- cart.php
+++ cart.php
@@ -1,5 +1,14 @@
 <?php
 include'config.php';
+
+//商品数を更新
+$a = ('update cart set  count = :count where customer_id =:customer_id and product_id =:product_id');
+  $a = $pdo->prepare($a);
+  $i = array(':count' =>$_POST['num'],':customer_id'=>$_SESSION['customer']['id'],':product_id'=>$_POST['id']);
+  //$a->bindValue(':count',$count);
+  //$a->bindValue(':customer_id',$customer_id);
+  //$a->bindValue('::product_id',$product_id);
+  $a->execute($i);
 ?>
 
 <?php include('head_foot/header.php');?>
