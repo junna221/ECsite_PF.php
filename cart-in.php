8e5b662772031b13a736fae89261a2065b671c7a
kaijunna <shell1016221@gmail.com>
2020-03-18 09:44:28 +0900

    no message

--- cart-in.php
+++ cart-in.php
@@ -1,11 +1,17 @@
 <?php include 'menu.php';
 
 $id=$_POST['id'];
-
+//カート変数が未定義の時カートをからの状態に初期化
+if($_SESSION['customer']['id']) {
+if (!isset($_SESSION['product'])) {
+  $_SESSION['product']=[];
+}
+//カートに入ってる個数を取得
 $count=0;
 if (isset($_SESSION['product'][$id])) {
   $count=$_SESSION['product'][$id]['num'];
 }
+//カートに商品を登録する
 $_SESSION['product'][$id]=[
   'name'=>$_POST['name'], 
   'price'=>$_POST['price'], 
